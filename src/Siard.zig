9555214d957c2f4bb4b0fcc1a4aaea8077515eb6
Richard Lehane <richard@itforarchivists.com>
2025-10-13 22:41:25 +1100

    start with a model

--- src/Siard.zig
+++ src/Siard.zig
@@ -1,1 +1,20 @@
 const std = @import("std");
+
+dbname: []const u8,
+dataOwnder: []const u8,
+dataOriginTimespan: []const u8,
+lobFolder: []const u8,
+producerApplication: []const u8,
+archivalData: []const u8,
+messageDigest: []const u8,
+clientMachine: []const u8,
+databaseProduct: []const u8,
+connection: []const u8,
+databaseUser: []const u8,
+
+const Schema = struct {
+    name: []const u8,
+    folder: []const u8,
+};
+
+const Table = struct {};
